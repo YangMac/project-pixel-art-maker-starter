4ecc36f9aae1d51fd9c07381db1be20607c47330
MacYang <yinnvc@gmail.com>
2019-02-27 23:12:42 +0800

    Update

--- designs.js
+++ designs.js
@@ -1,6 +1,10 @@
 // Select color input
 // Select size input
-
+const inputHeight = document.getElementById('inputHeight');
+const inputWidth = document.getElementById('inputWidth');
+const colorPicker = document.getElementById('colorPicker');
+const pixelCanvas = document.getElementById('pixelCanvas');
+const sizePicker = document.getElementById('sizePicker');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
